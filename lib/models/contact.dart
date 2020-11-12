f36ebc6a12def92d0a08371b6e57e265773825d1
Hugo Fusinato <hugohvf@hotmail.com>
2020-11-12 20:21:36 -0300

    Json Flutter Method

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -4,14 +4,24 @@ class Contact {
   final int accountNumber;
 
   Contact(
-      this.id,
-      this.name,
-      this.accountNumber,
-      );
+    this.id,
+    this.name,
+    this.accountNumber,
+  );
 
   @override
   String toString() {
     return 'Contact{id: $id, name: $name, accountNumber: $accountNumber}';
   }
 
+  Contact.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        accountNumber = json['accountNumver'];
+
+  Map<String, dynamic> toJson() =>
+      {
+        'name': name,
+        'accountNumber': accountNumber,
+      };
 }
